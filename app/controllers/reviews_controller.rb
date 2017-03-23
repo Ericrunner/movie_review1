e55743a0505d6eaabc0dc829a21dc1a021012965
liujia <ericrunner123456@yahoo.com>
2017-03-23 22:07:41 +0800

    add post

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,20 @@ end
 def new
   @review=Review.new
 end
+def show
+  @review=Review.find(params[:id])
+end
+def create
+  @review=Review.new(review_params)
+  if @review.save
+    redirect_to reviews_path
+  else
+    render :new
+  end
+end
+
+private
+def review_params
+  params.require(:review).permit(:title,:description)
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323020710) do
+ActiveRecord::Schema.define(version: 20170323140555) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "review_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.string   "title"
