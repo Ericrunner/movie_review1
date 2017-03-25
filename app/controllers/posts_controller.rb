f8929e5fe5107341a88aaf1df9aab66fe0ee4f61
liujia <ericrunner123456@yahoo.com>
2017-03-25 23:25:33 +0800

    adjust index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+before_action :authenticate_user! ,only: [:new,:create]
 def new
   @review=Review.find(params[:review_id])
   if current_user.is_favorites_of?(@review)
